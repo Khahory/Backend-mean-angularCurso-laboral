f8c5e79fea6fc8efeecc7d917b58c0fd63be8243
angelmaria <angeld05@live.com>
2019-09-12 14:33:10 -0400

    Actualizar usuario PUT

--- routes/usuario.js
+++ routes/usuario.js
@@ -6,8 +6,8 @@ var app = express();
 var Usuario = require('../models/usuario');
 
 
-<!-- ======================================================================== -->
 <!-- Start Obtener todos los usuarios  -->
+<!-- ======================================================================== -->
 
 // Rutasj
 app.get('/', (req, res, next) => {
@@ -30,11 +30,62 @@ app.get('/', (req, res, next) => {
         });
 });
 
+<!-- ======================================================================== -->
 <!-- End Obtener todos los usuarios  -->
+
+
+<!-- Start Actualizar un usuario -->
 <!-- ======================================================================== -->
 
+app.put('/:id', (req, res) => {
+    var id = req.params.id;
+    var body = req.body;
+
+    Usuario.findById(id, (err, usuario) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar  el usuario',
+                errors: err
+            });
+        }
+
+        if (!usuario) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'El usuario id: ' +id +' no existe',
+                errors: {message: 'No existe un usuario con ese ID'}
+            });
+        }
+
+        usuario.nombre = body.nombre;
+        usuario.email = body.email;
+        usuario.role = body.role;
+        usuario.save((err, usuarioGuardado) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar usuario',
+                    errors: err
+                });
+            }
+
+            usuarioGuardado.password = ':^D';
+
+            res.status(200).json({
+                ok: true,
+                usuario: usuarioGuardado
+            });
+        });
+    });
+});
+
 <!-- ======================================================================== -->
+<!-- End Actualizar un usuario -->
+
+
 <!-- Start Crear nuevo usuario -->
+<!-- ======================================================================== -->
 
 app.post('/', (req, res) => {
     var body = req.body;
@@ -64,8 +115,9 @@ app.post('/', (req, res) => {
     });
 });
 
-<!-- End Crear nuevo usuario -->
 <!-- ======================================================================== -->
+<!-- End Crear nuevo usuario -->
+
 
 //  Requerimos exportar esto porque lo usaremos en otro sitio
 module.exports = app;
